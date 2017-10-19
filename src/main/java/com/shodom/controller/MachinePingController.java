31d073aad339d3780d5278707fc5a586bfed3508
engin <engin@engin-MacBook-Pro.local>
2017-10-20 01:21:00 +0300

    düzenleme yapıldı

---
+++ src/main/java/com/shodom/controller/MachinePingController.java
@@ -0,0 +1,26 @@
+package com.shodom.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.shodom.model.MachinePing;
+import com.shodom.repository.MachinePingRepository;
+
+@RestController
+public class MachinePingController {
+
+	@Autowired
+	private MachinePingRepository repository;
+	
+	@RequestMapping(value = "/listMachines", method = RequestMethod.GET)
+	public ResponseEntity<?> listMachines() {
+		
+		return new ResponseEntity<List<MachinePing>>(repository.findAll(), HttpStatus.CREATED);
+    }
+}
